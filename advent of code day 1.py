d117513564c837276865ac3fed1c32228b518a62
higgouk <higgins169@gmail.com>
2025-12-02 21:10:02 +0000

    1.2 attempt

--- advent of code day 1.py
+++ advent of code day 1.py
@@ -1,16 +1,22 @@
 with open('data.txt', 'r') as data:
     instructions = data.read().strip().splitlines()
+
 current_number = 50
 zero_count = 0
+wheel_size = 100
 
 for line in instructions:
     direction = line[0]
-    value = int(line[1:])
+    distance = int(line[1:])
+   # print(f"direction: ", direction, "distance: ", distance)
+
     if direction == "R":
-        current_number = (current_number + value) % 100
+        zero_count += (current_number + distance) // wheel_size
+        current_number = (current_number + distance) % wheel_size
+       # print(f"zeros: ", zero_count, "current number: ", current_number)
     else:
-        current_number = (current_number - value) % 100
-    if current_number == 0:
-        zero_count += 1
+        zero_count += (distance - 1 + (wheel_size - current_number)) // wheel_size
+        current_number = (current_number - distance) % wheel_size
+    # print(f"direction: ", direction, "distance: ", distance)
 
 print(zero_count)
