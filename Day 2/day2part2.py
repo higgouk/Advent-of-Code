acf9573518deac8926d6723e03bb8623fd485214
higgouk <higgins169@gmail.com>
2025-12-03 17:35:12 +0000

    STUCK - string only slices into chunks of 1 and 2 then stops

--- Day 2/day2part2.py
+++ Day 2/day2part2.py
@@ -1,12 +1,11 @@
-with open('day2data', 'r') as data:
+with open('day2dataTEST', 'r') as data:
     product_ranges = data.read().strip().split(",")
 
 total = 0
 
-def equal_parts(s, n):
-    part_length = len(s) // n
-    parts = [s[i:i + part_length] for i in range(0, len(s), part_length)]
-    # print(parts)
+def equal_parts(string, part_length):
+    parts = [string[i:i + part_length] for i in range(0, len(string), part_length)]
+    print(parts)
     if len(set(parts)) == 1:
         return True
     return None
@@ -14,10 +13,10 @@ def equal_parts(s, n):
 for product_range in product_ranges:
     (start, end) = product_range.split("-")
     for product in range(int(start), int(end) + 1):
-        string = str(product)
-        length = len(string)
-        for l in range(2, length):
-            if equal_parts(string, l):
+        product_string = str(product)
+        length = len(product_string)
+        for n in range(1, length // 2):
+            if equal_parts(product_string, n):
                 total += product
 
 print(total)
