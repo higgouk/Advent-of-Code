9c3a48edd959964ca6e34abfa905c7872f6b9656
higgouk <higgins169@gmail.com>
2025-12-03 18:07:19 +0000

    SOLVED - ME ME A STUPIDMAN 💡

--- Day 2/day2part2.py
+++ Day 2/day2part2.py
@@ -1,11 +1,10 @@
-with open('day2dataTEST', 'r') as data:
+with open('day2data', 'r') as data:
     product_ranges = data.read().strip().split(",")
 
 total = 0
 
 def equal_parts(string, part_length):
     parts = [string[i:i + part_length] for i in range(0, len(string), part_length)]
-    print(parts)
     if len(set(parts)) == 1:
         return True
     return None
@@ -15,8 +14,9 @@ for product_range in product_ranges:
     for product in range(int(start), int(end) + 1):
         product_string = str(product)
         length = len(product_string)
-        for n in range(1, length // 2):
+        for n in range(1, (length // 2) + 1):
             if equal_parts(product_string, n):
                 total += product
+                break
 
 print(total)
