71c12a3c176c94aca5c808b92eca088c5202f766
higgouk <higgins169@gmail.com>
2025-12-04 17:22:58 +0000

    DONE THE LOT MATE

---
+++ Day 4/day4part2.py
@@ -0,0 +1,36 @@
+with open('day4data', 'r') as data:
+    rows = data.read().strip().splitlines()
+
+grid = [list(row) for row in rows]
+rows_number = len(grid)
+columns_number = len(grid[0])
+rolls_removed = 0
+
+def get_neighbours(row, column):
+    count = 0
+    for delta_row in (-1, 0, 1):
+        for delta_column in (-1, 0, 1):
+            if delta_row == 0 and delta_column == 0:
+                continue
+            new_row = row + delta_row
+            new_column = column + delta_column
+            if 0 <= new_row < rows_number and 0 <= new_column < columns_number:
+                if grid[new_row][new_column] == '@':
+                    count += 1
+    return count
+
+while True:
+    roll_batch = 0
+    for r in range(rows_number):
+        for c in range(columns_number):
+            if grid[r][c] == '.':
+                continue
+            elif get_neighbours(r, c) < 4:
+                grid[r][c] = '.'
+                roll_batch += 1
+    if roll_batch == 0:
+        break
+    else:
+        rolls_removed += roll_batch
+
+print(rolls_removed)
